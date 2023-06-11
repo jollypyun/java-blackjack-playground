d77dc90511a5a8cf9164a08fc7a89eedd926dddc
geunhyung <lucas.stave@gmail.com>
2023-06-11 22:07:22 +0900

    - 미션 1차 완료

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -27,7 +27,7 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        words.stream().filter(word -> word.length() > 12).distinct().sorted().sorted();
+        words.stream().filter(word -> word.length() > 12).distinct().sorted((a,b) -> b.length() - a.length()).limit(100).map(String::toLowerCase).forEach(System.out::println);
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
